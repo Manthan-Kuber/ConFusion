ec1ccb5685e41b27e9ebd81bd107f2ec99cdd18c
Manthan Kuber <manthankuber873@gmail.com>
2021-10-31 18:52:09 +0530

    Uncontrolled Forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -8,33 +8,54 @@ import {
   NavItem,
   NavbarBrand,
   NavbarToggler,
+  Modal,
+  Button,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
 class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            isNavOpen: false
-            
-        };
-        //this.toggleNav  = this.toggleNav.bind(this);
-        // Inorder to make the function available in our JSX, we need to bind this. Alternatively we can use an arrow function in the JSX itself instead of binding it 
-         this.toggleNav  = this.toggleNav.bind(this);
-    }
-    toggleNav(){
-        //We negate whaterver the state value maybe
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+    };
+    //this.toggleNav  = this.toggleNav.bind(this);
+    // Inorder to make the function available in our JSX, we need to bind this. Alternatively we can use an arrow function in the JSX itself instead of binding it
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  toggleNav() {
+    //We negate whaterver the state value maybe
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleLogin(event){
+      this.toggleModal();
+      alert('Username: ' + this.username.value + ' Password: ' + this.password.value + " Remember: " + this.remember.checked );
+      event.preventDefault();
+  }
   render() {
     return (
       //React Fragment ' <> </> ' or '<React.Fragment> </React.Fragment>' : Allows us to group together a bunch of react elements and then return it instead of enclosing it into one element (i.e inside a <div></div>). When we use a div for the same, a node gets added into the react DOM. But react fragment saves us a div as it does not get added into the DOM as a div
       <>
         <Navbar dark expand="md">
           <div className="container">
-              <NavbarToggler onClick={this.toggleNav}/>
+            <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto" href="/">
               <img
                 src="assets/images/logo.png"
@@ -67,6 +88,13 @@ class Header extends Component {
                   </NavLink>
                 </NavItem>
               </Nav>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span>Login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -85,6 +113,31 @@ class Header extends Component {
             </div>
           </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                {/* Using innerRef i.e references to retrieve values of the form */}
+                <Input type="text" id="username" name="username" innerRef={(input) => this.username=input } />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password" innerRef={(input) => this.password=input } />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember" innerRef={(input) => this.remember=input} />
+                  Remember Me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </>
     );
   }
